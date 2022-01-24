3ccc51e7e84b1825bfa56a95fdd0f83fad555f1e
pandaulait <show.hey.panda@gmail.com>
2022-01-24 22:37:50 +0900

    add Serializer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :projects
   root to: "home#top"
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
